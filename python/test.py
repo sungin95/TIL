7cbec99d66107b3bbea7ee0d2e9b870db8212a42
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-07-17 18:18:42 +0900

    0714이름변경

--- python/test.py
+++ python/test.py
@@ -1,1 +1,8 @@
-print("Hello World")
+numbers = [1, 2, 3, 4,100]
+print(numbers)
+# [1, 2, 3, 4]
+print(numbers.index(3))
+# 2 ( )
+print(numbers.index(100))
+# -----------------------
+# ValueError: 100 is not in list
