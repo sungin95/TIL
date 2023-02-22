722fd5ac80976969065c6c8e0ea2616c17725954
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-02-22 17:45:04 +0900

    0222

--- python/코드테스트(연습)/백준/23y02m/9663.py
+++ python/코드테스트(연습)/백준/23y02m/9663.py
@@ -1,30 +1,33 @@
 n = int(input())
 
-ans = 0
 row = [0] * n
+answer = 0
 
 
-def is_promising(x):
+# 왼쪽 위 검증 함수
+def is_check(x):
     for i in range(x):
+        # 열체크 or 대각선 체크
         if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
             return False
-
     return True
 
 
-def n_queens(x):
-    global ans
+def n_queen(x):
+    global answer
+    # 체스판 끝까지 다 채우면 카운트
+    # 모든 가로에는 반드시 퀀이 한개 있어야 하니까
     if x == n:
-        ans += 1
+        answer += 1
         return
-
     else:
+        # 모든 체스판에 퀀 한번씩 올려 보기
+        # 한줄에 한개라도 올려야 다음으로 넘어간다.
         for i in range(n):
-            # [x, i]에 퀸을 놓겠다.
             row[x] = i
-            if is_promising(x):
-                n_queens(x + 1)
+            if is_check(x):
+                n_queen(x + 1)
 
 
-n_queens(0)
-print(ans)
+n_queen(0)
+print(answer)
